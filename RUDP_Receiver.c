10df62b8a42f78c58de4f3ddecb2bf074fa06564
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2024-05-01 14:45:20 +0300

    Update RUDP_Receiver.c

--- RUDP_Receiver.c
+++ RUDP_Receiver.c
@@ -1,86 +1,99 @@
+#include "RUDP_API.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/time.h>
-#include "RUDP_API.h"
+
+typedef struct {
+    PacketType type;
+    unsigned int seq_num;
+    char data[1024];
+} RUDPPacket;
+
+int initiate_handshake(int sockfd, struct sockaddr_in *dest_addr) {
+    RUDPPacket packet = { .type = HANDSHAKE_INIT, .seq_num = 0 };
+    if (sendto(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *)dest_addr, sizeof(struct sockaddr_in)) < 0) {
+        perror("Handshake send failed");
+        return -1;
+    }
+
+    // Wait for handshake ACK
+    struct sockaddr_in from;
+    socklen_t fromlen = sizeof(from);
+    if (recvfrom(sockfd, &packet, sizeof(packet), 0, (struct sockaddr *)&from, &fromlen) < 0 || packet.type != HANDSHAKE_ACK) {
+        perror("Handshake ACK failed");
+        return -1;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <IP> <PORT>\n", argv[0]);
         return 1;
     }
 
-    int port = atoi(argv[1]); // Convert port number from string to integer
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+    char buffer[1024];
+    FILE *file;
 
-    // Create UDP socket
-    int sockfd = rudp_socket(AF_INET, SOCK_DGRAM, 0);
+    int sockfd = rudp_socket();
     if (sockfd < 0) {
         perror("Failed to create UDP socket");
         return 1;
     }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
-    server_addr.sin_port = htons(port); // Host to network short
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(port);
+    dest_addr.sin_addr.s_addr = inet_addr(ip);
 
-    // if (rudp_bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-    //     perror("Failed to bind socket");
-    //     rudp_close(sockfd);
-    //     return 1;
-    // }
+    if (initiate_handshake(sockfd, &dest_addr) != 0) {
+        fprintf(stderr, "Handshake failed, terminating.\n");
+        return 1;
+    }
 
-    char buffer[1024]; // Buffer to store received data
-    struct timeval start, end;
-    long mtime, seconds, useconds;
-    double total_bytes_received = 0;
-    double total_time = 0;
-    int total_received_messages = 0;
-
-    while (1) {
-        gettimeofday(&start, NULL);
-        ssize_t received = rudp_recv(sockfd, buffer, sizeof(buffer), 0);
-        gettimeofday(&end, NULL);
-
-        if (received < 0) {
-            perror("Failed to receive data");
+    do {
+        printf("Enter the filename to send: ");
+        fgets(buffer, 1024, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        printf("file: %s\n", buffer);
+        file = fopen(buffer, "rb");
+        if (file == NULL) {
+            perror("Failed to open file");
             continue;
         }
 
-        seconds  = end.tv_sec  - start.tv_sec;
-        useconds = end.tv_usec - start.tv_usec;
-        mtime = ((seconds) * 1000 + useconds / 1000.0) + 0.5;
-
-        if (received > 0) {
-            total_bytes_received += received;
-            total_time += mtime;
-            total_received_messages++;
-            buffer[received] = '\0'; // Null-terminate string
-            printf("Received: %s in %ld ms.\n", buffer, mtime);
+        while (!feof(file)) {
+            size_t read_bytes = fread(buffer, 1, sizeof(buffer), file);
+            if (ferror(file)) {
+                perror("Read error");
+                break;
+            }
+            if (read_bytes > 0) {
+                if (rudp_send(sockfd, buffer, read_bytes, 0, ip, port) < 0) {
+                    perror("Failed to send data");
+                }
+            }
         }
 
-        // Check for a termination condition
-        if (strcmp(buffer, "exit") == 0) {
-            printf("Exit signal received, shutting down.\n");
-            break;
-        }
-    }
+        fclose(file);
 
-    // Calculate and display the average time and bandwidth
-    if (total_received_messages > 0) {
-        double average_time = total_time / total_received_messages;
-        double average_bandwidth = (total_bytes_received / total_time) * 1000; // Bytes per second
-        printf("Average reception time: %.2f ms\n", average_time);
-        printf("Average bandwidth: %.2f Bytes/s\n", average_bandwidth);
-    }
+        printf("Send the file again? (yes/no): ");
+        fgets(decision, sizeof(decision), stdin);
+        decision[strcspn(decision, "\n")] = 0;  // Remove trailing newline
 
-    // Close the UDP socket
+    } while (strncmp(decision, "yes", 3) == 0);
+
+    strcpy(buffer, "exit");
+    rudp_send(sockfd, buffer, strlen(buffer), 0, ip, port);
     rudp_close(sockfd);
 
     return 0;
 }
+
