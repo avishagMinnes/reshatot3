1c1eaaed7dbe0118127cc9d06d91f6127e43f5b4
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2024-05-01 18:45:48 +0300

    Update RUDP_API.h

--- RUDP_API.h
+++ RUDP_API.h
@@ -11,14 +11,6 @@
 #include <time.h>
 #include <errno.h>
 
-
-#define TIMES_TO_SEND 100
-#define BITS_TO_BYTES 8
-
-#define TIMEOUT_MICROSECS 700000
-#define BUFFER_SIZE 65000
-#define SERVER_IP_ADDRESS "127.0.0.1"
-
 #define MAX_DATA_SIZE 1024
 
 typedef enum {
@@ -28,17 +20,11 @@ typedef enum {
 typedef struct {
     PacketType type;
     unsigned int seq_num;
-    char data[1024];
-} RUDPPacket;
-
-
-typedef struct {
     uint16_t length;
     uint16_t checksum;
     uint8_t flags;
     char data[MAX_DATA_SIZE];
 } RUDP_Packet;
-
 
 
 // Allocates a new structure for the RUDP socket (contains basic information about the socket itself). Also creates a UDP socket as a baseline for the RUDP.
@@ -51,13 +37,10 @@ int rudp_recv(int sockfd, void *buf, size_t len, int flags, int port);
 
 // Sends data stores in buffer to the other side. Returns the number of sent bytes on success, 0 if got FIN packet (disconnect), and -1 on error. Fails if called when the socket is disconnected.
 int rudp_send(int sockfd, const void *buf, short len, int flags, const char *ip, int port);
-// Disconnects from an actively connected socket. Returns 1 on success, 0 when the socket is already disconnected (failure).
-//int rudp_disconnect(RUDP_Socket *sockfd);
 
 // This function releases all the memory allocation and resources of the socket.
 int rudp_close(int sockfd);
 
 //this function calculates the checksum
 unsigned short int calculate_checksum(void *data, unsigned int bytes);
-
 
